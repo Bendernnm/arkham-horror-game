b1a02de796db9461584055a20434d08249a1f535
bender <bender.d1mon@gmail.com>
2020-06-04 12:54:44 +0300

    add myth and add to pull

--- public/index.js
+++ public/index.js
@@ -1,4 +1,3 @@
-const cache = {};
 const addedMyths = [];
 const mythsPull = [];
 const randomMyths = [];
@@ -34,50 +33,108 @@ const myths = [
 ];
 
 function addMyth2Pull(myth) {
+  const mythCopy = { ...myth };
+  const $mythsPull = document.getElementById('myths-pull');
+  const mythDiv = document.createElement('div');
+  const remove = document.createElement('button');
+  const { h2, img } = htmlElMyth(mythCopy);
 
+  remove.innerText = 'Видалити з пуллу';
+  remove.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const mythsIndex = mythsPull.indexOf(mythCopy);
+
+    if (mythsIndex === -1) {
+      return;
+    }
+
+    mythsPull.splice(mythsIndex, 1);
+    mythDiv.remove();
+  });
+
+  mythDiv.append(h2, img, remove);
+
+  $mythsPull.append(mythDiv);
+  mythsPull.push(mythCopy);
+}
+
+function addMyth(myth) {
+  const mythCopy = { ...myth };
+  const $allMyth = document.getElementById('all-myths');
+  const mythDiv = document.createElement('div');
+  const remove = document.createElement('button');
+  const addToPullBtn = document.createElement('button');
+  const { h2, img } = htmlElMyth(mythCopy);
+
+  remove.innerText = 'Видалити';
+  remove.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const addedMythsIndex = addedMyths.indexOf(mythCopy);
+
+    if (addedMythsIndex === -1) {
+      return;
+    }
+
+    addedMyths.splice(addedMythsIndex, 1);
+    mythDiv.remove();
+  });
+
+  addToPullBtn.innerText = 'Додати до пуллу';
+  addToPullBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    addMyth2Pull(mythCopy);
+  });
+
+  mythDiv.append(h2, img, remove, addToPullBtn);
+
+  $allMyth.append(mythDiv);
+  addedMyths.push(mythCopy);
+
+  addMyth2Pull(mythCopy);
+}
+
+function htmlElMyth(myth) {
+  const h2 = document.createElement('h2');
+  const img = document.createElement('img');
+
+  h2.innerText = myth.name;
+
+  img.setAttribute('weight', '50');
+  img.setAttribute('height', '50');
+  img.setAttribute('src', `imgs/${myth.url}`);
+
+  return { h2, img };
 }
 
 function showBasicMyths() {
-  cache.basicMyth = {};
-
-  const els = [];
-  const mythsList = document.getElementById('myths-list');
+  const $mythsList = document.getElementById('myths-list');
 
   myths.forEach((myth) => {
     const div = document.createElement('div');
-    const h2 = document.createElement('h2');
-    const img = document.createElement('img');
-    const addToPull = document.createElement('button');
+    const add = document.createElement('button');
+    const { h2, img } = htmlElMyth(myth);
 
-    h2.innerText = myth.name;
-
-    img.setAttribute('weight', '50');
-    img.setAttribute('height', '50');
-    img.setAttribute('src', `imgs/${myth.url}`);
-
-    addToPull.innerText = 'Додати до пулу';
-    addToPull.addEventListener('onclick', (e) => {
+    add.innerText = 'Додати';
+    add.addEventListener('click', (e) => {
       e.preventDefault();
 
-      addMyth2Pull(myth);
+      addMyth(myth);
     });
 
-    div.append(h2);
-    div.append(img);
-    div.append(addToPull);
+    div.append(h2, img, add);
 
-    mythsList.append(div);
-
-    els.push({ h2, img, addToPull });
+    $mythsList.append(div);
   });
-
-  cache.basicMyth = { els, mythsList };
 }
 
 function init() {
-
+  showBasicMyths();
 }
 
+
 window.onload = function() {
-  showBasicMyths();
+  init();
 };
